4bd2e581bdea833fcc0ab1cd2c7eca3b1577f875
Marika Marszalkowski <marika.marszalkowski@sap.com>
2019-11-19 12:43:48 +0100

    Add version

--- types/http-proxy-agent/index.d.ts
+++ types/http-proxy-agent/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for http-proxy-agent 2.0
+// Type definitions for http-proxy-agent 2.1
 // Project: https://github.com/TooTallNate/node-http-proxy-agent
 // Definitions by: mrmlnc <https://github.com/mrmlnc>
 //                 steprescott <https://github.com/steprescott>
